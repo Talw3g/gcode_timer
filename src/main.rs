78c32687ecf3839949164f4ad41e896690d8e96e
Thibault <tmarion90@gmail.com>
2018-03-26 19:22:52 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#![allow(unused)]
+#![feature(io)]
+
+#[macro_use]
+extern crate error_chain;
+extern crate walkdir;
+extern crate read_lines;
+
+mod other_error {
+    error_chain!{}
+}
+
+error_chain!{
+
+    links {
+        Another(other_error::Error, other_error::ErrorKind) #[cfg(unix)];
+    }
+
+}
+
+
+//use std::fs;
+use std::path::PathBuf;
+use read_lines::read_line;
+//use std::fmt;
+
+
+fn main() {
+
+    if let Err(ref e) = run() {
+
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "error: {}", e).expect(errmsg);
+        for e in e.iter().skip(1) {
+            writeln!(stderr, "caused by: {}", e).expect(errmsg);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+
+
+
+fn run() -> Result<()> {
+    let mut liner = read_line::lines(PathBuf::from("/home/thibault/shared/testwin.txt"))
+        .chain_err(|| "Error creating LineReader")?;
+    for line in liner {
+        let line = line
+            .chain_err(|| "Error reading line")?;
+        println!("{}",line);
+    }
+    println!("Reached EOF");
+    Ok(())
+}
